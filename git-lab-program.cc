7823d2570a6eac47ac0f68b58e8f4b8b42de6fdd
Anthony3714 <ag168918@ohio.edu>
2020-01-22 17:52:45 -0500

    C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Anthony Giordano>
+ *        Date: <January 22, 2020>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
